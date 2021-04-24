6988a17a527e39e92e3322d49c5ad840fe18a59b
Nicolas Unterberger <nicounterberger@yahoo.at>
2021-04-24 11:24:55 +0200

    Merge Sort fertig

--- src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
+++ src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
@@ -78,6 +78,52 @@ public class Ab1Impl implements Ab1 {
 
 	@Override
 	public void mergesort(int[] data) {
-
+// YOUR CODE HERE
+		int length = data.length;
+		int[] sorted = mergeSort(data, 0, length - 1);
+		System.arraycopy(sorted, 0, data, 0, length);
 	}
-}
+
+	private int[] mergeSort(int[] elements, int left, int right) {
+		//Ende erreicht?`
+		if (left == right) return new int[]{elements[left]};
+
+		int middle = left + (right - left) / 2;
+		int[] leftArray = mergeSort(elements, left, middle);
+		int[] rightArray = mergeSort(elements, middle + 1, right);
+		return merge(leftArray, rightArray);
+	}
+
+	int[] merge(int[] leftArray, int[] rightArray) {
+		int leftLen = leftArray.length;
+		int rightLen = rightArray.length;
+
+		int[] target = new int[leftLen + rightLen];
+		int targetPos = 0;
+		int leftPos = 0;
+		int rightPos = 0;
+
+		// solange beide arrays elemente beinhalten
+		while (leftPos < leftLen && rightPos < rightLen) {
+			// Welches ist kleiner?
+			int leftValue = leftArray[leftPos];
+			int rightValue = rightArray[rightPos];
+			if (leftValue <= rightValue) {
+				target[targetPos++] = leftValue;
+				leftPos++;
+			} else {
+				target[targetPos++] = rightValue;
+				rightPos++;
+			}
+		}
+		// restliches kopieren...
+		while (leftPos < leftLen) {
+			target[targetPos++] = leftArray[leftPos++];
+		}
+		while (rightPos < rightLen) {
+			target[targetPos++] = rightArray[rightPos++];
+		}
+		return target;
+	}
+	}
+
